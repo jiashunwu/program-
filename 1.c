92e9ccbb984055328eca922c63fc1459bdb5cdd2
wjs <wjs@localhost.localdomain>
2015-03-21 09:49:07 -0700

    1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: wujiashun
+    > Mail: 970069059@qq.com 
+    > Created Time: Sat 21 Mar 2015 09:48:31 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+fjkdsfhjkl
+fdajfgd
+ag
+dfsg
+dfs
+
